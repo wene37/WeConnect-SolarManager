1d746323bd2b81ebcb15616d6408369539be98e7
Thomas Werner <thomas.werner@witro.ch>
2022-09-20 20:56:23 +0200

    Enhanced setup.py

--- setup.py
+++ setup.py
@@ -13,19 +13,26 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="WeConnect-SolarManager",
-    version="0.2.1",
+    version="0.2.10",
     description="With WeConnect-SolarManager you can automatically charge your Volkswagen ID car (e.g. ID.4) with solar electricity, even if your wallbox does not support this.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/wene37/WeConnect-SolarManager",
     author="Thomas Werner",
-    author_email="thomas.werner@witro.ch",
     classifiers=[
         "License :: OSI Approved :: MIT License"
     ],
     package_dir={"": "src"},
     packages=find_packages(where="src"),
+    data_files=[
+        ('SolarManager', ['src/main.py', 'src/config.txt']),
+        ('SolarManager/logs', ['src/logs/.gitkeep'])
+    ],
     python_requires=">=3.7, <4",
+    #install_requires=[
+    #    'requests >= 2.27.1',
+    #    'weconnect[Images] >= 0.48.0'
+    #],
     project_urls={
         "Bug Reports": "https://github.com/wene37/WeConnect-SolarManager/issues",
         "Source": "https://github.com/wene37/WeConnect-SolarManager"
